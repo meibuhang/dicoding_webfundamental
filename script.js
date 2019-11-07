5a2ab1e80255c691c162d6c65edcab7e49022d2e
meibuhang <sitimelina@gmail.com>
2019-11-07 13:03:02 +0800

    final dicoding web fundamental

---
+++ script.js
@@ -0,0 +1,22 @@
+const prghp=document.getElementsByTagName('p');
+for (let x=0; x<prghp.length;x++)
+{
+  prghp[0].innerHTML="Seluruh kota merupakan tempat bermain yang asik, Oh Senangnya, aku Senang Sekali. Kalau begini akupun jadi sibuk, berusaha mengejar-ngejar dia";
+  prghp[1].innerHTML="Matahari menyinari semua perasaan cinta, tapi mengapa hanya aku yang di marahi ?";
+  prghp[2].innerHTML="Di musim panas, merupakan hari bermain gembira, sang gajah terkena Flu , pilek tiada henti-hentinya, sang beruang tidur dan tak ada yang berani ganggu dia, oh sibuknya. aku sibuk sekali";
+}
+
+const list = document.querySelectorAll('#listInformation li')
+for (let i=0; i<list.length; i++)
+  {
+    list[0].innerHTML="Name : <span> Siti Melina Buhang </span>";
+    list[1].innerHTML="Job Profession: <span> Flower Artist </span>";
+    list[2].innerHTML="Date Of Birth: <span> 05 Mei 1993 </span>";
+    list[3].innerHTML="Age : <span> 26 Years Old</span>";
+    list[4].innerHTML="Phone : <span> +62 853 9914 0190</span>";
+    list[5].innerHTML="Email : <span> sitimelina@gmail.com</span>";
+  }
+  
+
+
+  
